db5b350c4123a344720ca5e6b3fab75e5a95ee76
Dariusz Ochotny <dariuszochotny9@gmail.com>
2021-05-06 17:58:26 +0200

    Add initial style sheets

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,16 @@
 import '../styles/global.scss'
+import useLocalStor from '../components/local-storage';
+import { Span, State, Main } from '../components/styled-comp';
+import styles from '../styles/mainpage.module.scss';
+import lanStyles from '../styles/language.module.scss';
+import React, { useState, useEffect } from 'react';
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+
+    <Component {...pageProps} />
+
+  )
 }
 
 export default MyApp
--- pages/_document.js
+++ pages/_document.js
@@ -1,9 +1,17 @@
 import Document, { Html, Head, Main, NextScript } from 'next/document';
+import { ServerStyleSheet } from 'styled-components';
+
 
 class MyDocument extends Document {
-  static async getInitialProps(ctx) {
-    const initialProps = await Document.getInitialProps(ctx)
-    return { ...initialProps }
+  // static async getInitialProps(ctx) {
+  //   const initialProps = await Document.getInitialProps(ctx)
+  //   return { ...initialProps }
+  // }
+  static getInitialProps({ renderPage }) {
+    const sheet = new ServerStyleSheet()
+    const page = renderPage(App => props => sheet.collectStyles(<App {...props} />))
+    const styleTags = sheet.getStyleElement()
+    return { ...page, styleTags }
   }
 
   render() {
@@ -19,6 +27,7 @@ class MyDocument extends Document {
           <link href="https://fonts.googleapis.com/css2?family=Oswald&family=Roboto+Condensed&display=swap" rel="stylesheet" />
           <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.15.1/css/all.css"
             integrity="sha384-vp86vTRFVJgpjF9jiIGPEEqYqlDwgyBgEF109VFjmqGmIY/Y4HV4d3Gp2irVfcrp" crossOrigin="anonymous"></link>
+          {this.props.styleTags}
         </Head>
         <body>
           <Main />
